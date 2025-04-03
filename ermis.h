82bb3fc9aebcd262b71741c7aff841cb2125b155
OlehKniaziev <atmatm9182@gmail.com>
2025-04-03 21:57:55 +0200

    Added hashmaps to the `ermis` library

--- ermis.h
+++ ermis.h
@@ -12,7 +12,34 @@
         T *items;                                                       \
         UZ count;                                                       \
         UZ capacity;                                                    \
-    } arrname;
+    } arrname;                                                          \
+                                                                        \
+    void arrname##Init(arrname *arr, HeliosAllocator allocator, UZ cap); \
+    void arrname##Push(arrname *arr, T item);                           \
+                                                                        \
+    HELIOS_INLINE T arrname##Pop(arrname *arr) {                        \
+        HELIOS_VERIFY(arr->count != 0);                                 \
+        return arr->items[--arr->count];                                \
+    }                                                                   \
+                                                                        \
+    HELIOS_INLINE T arrname##At(arrname *arr, UZ idx) {                 \
+        HELIOS_VERIFY(idx < arr->count);                                \
+        return arr->items[idx];                                         \
+    }                                                                   \
+                                                                        \
+    HELIOS_INLINE T *arrname##AtP(arrname *arr, UZ idx) {               \
+        HELIOS_VERIFY(idx < arr->count);                                \
+        return &arr->items[idx];                                        \
+    }                                                                   \
+                                                                        \
+    HELIOS_INLINE const T *arrname##AtPC(const arrname *arr, UZ idx) {  \
+        HELIOS_VERIFY(idx < arr->count);                                \
+        return &arr->items[idx];                                        \
+    }                                                                   \
+                                                                        \
+    HELIOS_INLINE void arrname##Free(arrname *arr) {                    \
+        HeliosFree(arr->allocator, arr->items, sizeof(T) * arr->capacity); \
+    }
 
 #define ERMIS_IMPL_ARRAY(T, arrname)                                    \
     void arrname##Init(arrname *arr, HeliosAllocator allocator, UZ cap) { \
@@ -24,36 +51,112 @@
                                                                         \
     void arrname##Push(arrname *arr, T item) {                          \
         if (arr->count >= arr->capacity) {                              \
-            UZ new_capacity = ERMIS_ARRAY_GROW_FACTOR(arr->capacity);  \
+            UZ new_capacity = ERMIS_ARRAY_GROW_FACTOR(arr->capacity);   \
             arr->items = HeliosRealloc(arr->allocator, arr->items, sizeof(T) * arr->capacity, sizeof(T) * new_capacity); \
             arr->capacity = new_capacity;                               \
         }                                                               \
                                                                         \
         arr->items[arr->count++] = item;                                \
-    }                                                                   \
-                                                                        \
-    T arrname##Pop(arrname *arr) {                                      \
-        HELIOS_VERIFY(arr->count != 0);                                 \
-        return arr->items[--arr->count];                                \
-    }                                                                   \
-                                                                        \
-    T arrname##At(arrname *arr, UZ idx) {                               \
-        HELIOS_VERIFY(idx < arr->count);                                \
-        return arr->items[idx];                                         \
-    }                                                                   \
-                                                                        \
-    T *arrname##AtP(arrname *arr, UZ idx) {                             \
-        HELIOS_VERIFY(idx < arr->count);                                \
-        return &arr->items[idx];                                        \
-    }                                                                   \
-                                                                        \
-    const T *arrname##AtPC(const arrname *arr, UZ idx) {                \
-        HELIOS_VERIFY(idx < arr->count);                                \
-        return &arr->items[idx];                                        \
-    }                                                                   \
-                                                                        \
-    HELIOS_INLINE void arrname##Free(arrname *arr) {                    \
-        HeliosFree(arr->allocator, arr->items, sizeof(T) * arr->capacity); \
     }
 
+#define ERMIS_DECL_HASHMAP(K, V, hashmapname) typedef struct hashmapname { \
+        K *keys;                                                        \
+        V *values;                                                      \
+        U8 *meta;                                                       \
+        UZ capacity;                                                    \
+        UZ count;                                                       \
+        HeliosAllocator allocator;                                      \
+    } hashmapname;                                                      \
+                                                                        \
+    void hashmapname##Init(hashmapname *map, HeliosAllocator allocator, UZ cap); \
+    B32 hashmapname##Insert(hashmapname *map, K key, V value);          \
+    B32 hashmapname##Find(hashmapname *map, K key, V *value);           \
+                                                                        \
+    HELIOS_INLINE void hashmapname##Free(hashmapname *map) {            \
+        HeliosFree(map->allocator, map->keys, sizeof(K) * map->capacity); \
+        HeliosFree(map->allocator, map->values, sizeof(V) * map->capacity); \
+        HeliosFree(map->allocator, map->meta, sizeof(map->meta[0]) * map->capacity); \
+    }
+
+#define ERMIS_HASHMAP_DEFAULT_CAP (47)
+
+#define ERMIS_HASHMAP_GROW_FACTOR(x) ((x) * 3)
+
+#define ERMIS_HASH_OCCUPIED (1 << 0)
+
+#define ERMIS_HASHMAP_FOREACH(hashmap, keyname, valuename, body)        \
+    for (UZ _idx = 0; _idx < (hashmap)->capacity; ++_idx) {             \
+        if (((hashmap)->meta[_idx] & ERMIS_HASH_OCCUPIED) == 0) continue; \
+        typeof((hashmap)->keys[0]) keyname = (hashmap)->keys[_idx];     \
+        typeof((hashmap)->values[0]) valuename = (hashmap)->values[_idx]; \
+        body;                                                           \
+    }
+
+#define ERMIS_IMPL_HASHMAP(K, V, hashmapname, eqfunc, hashfunc)         \
+    void hashmapname##Init(hashmapname *map, HeliosAllocator allocator, UZ cap) { \
+        map->allocator = allocator;                                     \
+        map->keys = HeliosAlloc(allocator, sizeof(K) * cap);            \
+        map->values = HeliosAlloc(allocator, sizeof(V) * cap);          \
+        map->meta = HeliosAlloc(allocator, sizeof(map->meta[0]) * cap); \
+        map->capacity = cap ? cap : ERMIS_HASHMAP_DEFAULT_CAP;          \
+        map->count = 0;                                                 \
+    }                                                                   \
+                                                                        \
+    int hashmapname##Insert(hashmapname *map, K key, V value) {         \
+        UZ load_percentage = map->count * 100 / map->capacity;          \
+        if (load_percentage >= 70) {                                    \
+            UZ new_cap = ERMIS_HASHMAP_GROW_FACTOR(map->capacity);      \
+            hashmapname new_map;                                        \
+            hashmapname##Init(&new_map, map->allocator, new_cap);       \
+                                                                        \
+            ERMIS_HASHMAP_FOREACH(map, k, v, {                          \
+                    hashmapname##Insert(&new_map, k, v);                \
+                });                                                     \
+            hashmapname##Free(map);                                     \
+            *map = new_map;                                             \
+        }                                                               \
+                                                                        \
+        U64 idx = hashfunc(key) % map->capacity;                        \
+                                                                        \
+        while (1) {                                                     \
+            if ((map->meta[idx] & ERMIS_HASH_OCCUPIED) == 0) {          \
+                map->keys[idx] = key;                                   \
+                map->values[idx] = value;                               \
+                map->meta[idx] |= ERMIS_HASH_OCCUPIED;                  \
+                ++map->count;                                           \
+                return 1;                                               \
+            }                                                           \
+                                                                        \
+            if (eqfunc(map->keys[idx], key)) {                          \
+                map->keys[idx] = key;                                   \
+                map->values[idx] = value;                               \
+                return 0;                                               \
+            }                                                           \
+                                                                        \
+            ++idx;                                                      \
+        }                                                               \
+    }                                                                   \
+                                                                        \
+    B32 hashmapname##Find(hashmapname *map, K key, V *value) {          \
+        U64 idx = hashfunc(key) % map->capacity;                        \
+        U64 start_idx = idx;                                            \
+                                                                        \
+        do {                                                            \
+            if ((map->meta[idx] & ERMIS_HASH_OCCUPIED) && eqfunc(key, map->keys[idx])) { \
+                *value = map->values[idx];                              \
+                return 1;                                               \
+            }                                                           \
+                                                                        \
+            ++idx;                                                      \
+            /* NOTE: using an `if` instead of modulus */                \
+            if (idx > map->capacity) idx = 0;                           \
+        } while (idx != start_idx);                                     \
+                                                                        \
+        return 0;                                                       \
+    }
+
+// Equality and hash functions
+
+HELIOS_INLINE B32 ErmisEqFuncU32(U32 lhs, U32 rhs) { return lhs == rhs; }
+HELIOS_INLINE U64 ErmisHashFuncU32(U32 x) { return (U64)x; }
 #endif // ASTRON_ERMIS_H
--- tests/ermis.c
+++ tests/ermis.c
@@ -5,7 +5,10 @@
 ERMIS_DECL_ARRAY(S32, IntArray)
 ERMIS_IMPL_ARRAY(S32, IntArray)
 
-int main(void) {
+ERMIS_DECL_HASHMAP(U32, U32, IntsMap)
+ERMIS_IMPL_HASHMAP(U32, U32, IntsMap, ErmisEqFuncU32, ErmisHashFuncU32)
+
+void test_array(void) {
     UZ ints_count = 10;
 
     HeliosAllocator malloc_allocator = HeliosNewMallocAllocator();
@@ -23,6 +26,30 @@ int main(void) {
     for (UZ i = 0; i < ints_count; ++i) {
         HELIOS_VERIFY(IntArrayAt(&ints, i) == (S32)i);
     }
+}
 
-    return 0;
+void test_hashmap(void) {
+    HeliosAllocator malloc_allocator = HeliosNewMallocAllocator();
+    IntsMap map;
+    IntsMapInit(&map, malloc_allocator, 500);
+
+    HELIOS_VERIFY(map.count == 0);
+    HELIOS_VERIFY(map.capacity == 500);
+
+    for (U32 i = 0; i < 350; ++i) {
+        HELIOS_VERIFY(IntsMapInsert(&map, i, i * 2));
+    }
+
+    HELIOS_VERIFY(map.capacity == 500);
+    HELIOS_VERIFY(map.count == 350);
+
+    HELIOS_VERIFY(IntsMapInsert(&map, 1000, 2000));
+
+    HELIOS_VERIFY(map.capacity == ERMIS_HASHMAP_GROW_FACTOR(500));
+    HELIOS_VERIFY(map.count == 351);
+}
+
+int main() {
+    test_array();
+    test_hashmap();
 }
