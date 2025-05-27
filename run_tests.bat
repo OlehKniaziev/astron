de0cdcf894c0255f44986495f33ccb69ff6e974a
OlehKniaziev <atmatm9182@gmail.com>
2025-05-27 16:02:44 +0200

    Added undefined behavior sanitizers to compiler options in test scripts

--- run_tests.bat
+++ run_tests.bat
@@ -1,4 +1,4 @@
 for %%f in (.\tests\*.c) do (
-    clang -O0 -g -Wall -Wextra -pedantic -Werror -o "%%f.exe" %%f
+    clang -fsanitize=undefined -g -Wall -Wextra -pedantic -Werror -o "%%f.exe" %%f
     ".\%%f.exe"
 )
