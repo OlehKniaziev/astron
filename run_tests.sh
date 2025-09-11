dd3b92a9cb4e62c59ab5b06b292893ff5630f89c
OlehKniaziev <atmatm9182@gmail.com>
2025-09-11 20:12:46 +0200

    Changed the test script to include debug info

--- run_tests.sh
+++ run_tests.sh
@@ -5,8 +5,7 @@ set -e
 CC="${CC:-cc}"
 
 for test_file in ./tests/*.c; do
-    $CC -fsanitize=undefined -Wall -Wextra -pedantic -Werror -o "$test_file.o" $test_file
+    $CC -fsanitize=undefined -g -O0 -Wall -Wextra -pedantic -Werror -o "$test_file.o" $test_file
     ./"$test_file.o" || (echo "Failed to run test $test_file" && exit 1)
     echo "Ran test $test_file successfully"
-    rm ./"$test_file.o"
 done
